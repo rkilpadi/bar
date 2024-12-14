1cfbffc3c366f4f6af07d934550fc8a68290eda5
rkilpadi <ryan@kilpadi.com>
2024-12-14 02:19:03 +0100

    Implement live question feed

--- server.go
+++ server.go
@@ -16,11 +16,6 @@ import (
 const redisAddr = "localhost:6379"
 const serverPort = ":3000"
 
-type Data struct {
-    Avg float64   `redis:"avg"`
-    NumVotes int64  `redis:"numVotes"`
-}
-
 func initializeRedis() (context.Context, *redis.Client, error) {
     ctx := context.Background()
     r := redis.NewClient(&redis.Options{
@@ -33,14 +28,14 @@ func initializeRedis() (context.Context, *redis.Client, error) {
         return nil, nil, err
     }
 
-    err := r.HSet(ctx, "session:test", "avg", "0", "numVotes", "0").Err(); if err != nil {
+    err := r.MSet(ctx, "confidence", 0, "numVotes", 0).Err(); if err != nil {
         return nil, nil, err
     }
 
     return ctx, r, nil
 }
 
-func formatBar(data Data) string {
+func formatBar(confidence float64, numVotes int) string {
     rawHtml := fmt.Sprintf(`
         <div id="progress-bar">
             <div id="progress" style="background-color: hsl(%f, 70%%, 50%%); width: %f%%;">
@@ -48,9 +43,23 @@ func formatBar(data Data) string {
             </div>
         </div>
         <p>Total votes: %d</p>`,
-        data.Avg, data.Avg, int(math.Round(data.Avg)), data.NumVotes)
+        confidence, confidence, int(math.Round(confidence)), numVotes)
 
     return strings.ReplaceAll(rawHtml, "\n", "")
+}
+
+func formatQuestions(questions []string, page int64) string {
+    var htmlBuilder strings.Builder
+    var triggerAttributes string
+
+    for i, question := range questions {
+        if i == len(questions) - 1 {
+            triggerAttributes = fmt.Sprintf("hx-get=\"/question?page=%d\" hx-trigger=\"revealed\" hx-swap=\"afterend\"", page + 1)
+        }
+        htmlBuilder.WriteString(fmt.Sprintf("<p %s>%s</p>", triggerAttributes, question))
+    }
+
+    return htmlBuilder.String()
 }
 
 func main() {
@@ -77,13 +86,15 @@ func main() {
     e.File("/", "index.html")
 
     e.GET("/bar", func(c echo.Context) error {
-        var data Data
-
-        err := r.HGetAll(rctx, "session:test").Scan(&data); if err != nil {
+        confidence, err := r.Get(rctx, "confidence").Float64(); if err != nil {
             return echo.NewHTTPError(500, err)
         }
 
-        return c.HTML(200, formatBar(data))
+        numVotes, err := r.Get(rctx, "numVotes").Int(); if err != nil {
+            return echo.NewHTTPError(500, err)
+        }
+
+        return c.HTML(200, formatBar(confidence, numVotes))
     })
 
     e.POST("/vote", func(c echo.Context) error {
@@ -91,16 +102,13 @@ func main() {
             return echo.NewHTTPError(400, fmt.Sprintf("Error parsing confidence: %v", err))
         }
         if vote < 0 || vote > 100 {
-            return echo.NewHTTPError(400, fmt.Sprintf("Invalid confidence: %f", vote))
+            return echo.NewHTTPError(400, fmt.Sprintf("Confidence expected to be between 0-100, found: %f", vote))
         }
 
         scriptBytes, err := os.ReadFile("vote.lua"); if err != nil {
             echo.NewHTTPError(500, fmt.Sprintf("Failed to read script: %v", err))
         }
-        result, err := redis.NewScript(string(scriptBytes)).Run(rctx, r, 
-            []string{"session:test"}, 
-            c.RealIP(), vote,
-        ).StringSlice()
+        result, err := redis.NewScript(string(scriptBytes)).Run(rctx, r, []string{}, c.RealIP(), vote).StringSlice()
         if err != nil {
             return echo.NewHTTPError(500, fmt.Sprintf("Error running script: %v", err))
         }
@@ -115,22 +123,59 @@ func main() {
             return echo.NewHTTPError(500, err)
         }
 
-        html := formatBar(Data{newAvg, totalVotes})
-        err = r.Publish(rctx, "bar", html).Err(); if err != nil {
-            return err
+        html := formatBar(newAvg, int(totalVotes))
+        payload := fmt.Sprintf("event: vote\ndata: %s\n\n", html)
+        err = r.Publish(rctx, "sse", payload).Err(); if err != nil {
+            return echo.NewHTTPError(500, err)
         }
 
         return c.HTML(200, html)
     })
 
-    e.GET("/barstream", func(c echo.Context) error {
+    e.GET("/question", func(c echo.Context) error {
+        const pageLength = int64(30)
+
+        page, err := strconv.ParseInt(c.QueryParam("page"), 10, 64)
+        if err != nil {
+            return echo.NewHTTPError(400, fmt.Sprintf("Error parsing page: %v", err))
+        }
+        if page < 1 {
+            return echo.NewHTTPError(400, fmt.Sprintf("Page expected to be a positive integer, found: %d", page))
+        }
+
+        questions, err := r.ZRevRange(rctx, "questions", pageLength * (page - 1), page * pageLength - 1).Result()
+        if err != nil {
+            return echo.NewHTTPError(500, err)
+        }
+
+        return c.HTML(200, formatQuestions(questions, page))
+    })
+
+    e.POST("/question", func(c echo.Context) error {
+        timestamp := time.Now().Local().Format("15:04:05")
+        questionText := fmt.Sprintf("%s: %s", timestamp, c.FormValue("question"))
+        
+        err := r.ZAdd(rctx, "questions", 
+            redis.Z{Member: questionText, Score: float64(time.Now().Unix())},
+        ).Err(); if err != nil {
+            return echo.NewHTTPError(500, err)
+        }
+
+        payload := fmt.Sprintf("event: question\ndata: <p>%s</p>\n\n", questionText)
+        err = r.Publish(rctx, "sse", payload).Err(); if err != nil {
+            return echo.NewHTTPError(500, err)
+        }
+        return c.NoContent(202)
+    })
+
+    e.GET("/sse", func(c echo.Context) error {
         fmt.Printf("Connection from %s\n", c.Request().RemoteAddr)
 
         c.Response().Header().Set("Content-Type", "text/event-stream")
         c.Response().Header().Set("Cache-Control", "no-cache")
         c.Response().Header().Set("Connection", "keep-alive")
 
-        pubsub := r.Subscribe(rctx, "bar")
+        pubsub := r.Subscribe(rctx, "sse")
         defer pubsub.Close()
 
         ticker := time.NewTicker(time.Second)
@@ -144,7 +189,7 @@ func main() {
                 msg, err := pubsub.ReceiveMessage(rctx); if err != nil {
                     return err
                 }
-                fmt.Fprintf(c.Response(), "data: %s\n\n", msg.Payload)
+                fmt.Fprint(c.Response(), msg.Payload)
                 c.Response().Flush()
             }
         }
--- vote.lua
+++ vote.lua
@@ -1,22 +1,28 @@
-local sessionKey = KEYS[1]
 local ip = ARGV[1]
 local vote = tonumber(ARGV[2])
 
-local avg = tonumber(redis.call('HGET', sessionKey, 'avg') or "0")
-local numVotes = tonumber(redis.call('HGET', sessionKey, 'numVotes') or "0")
-local ipExists = redis.call('HEXISTS', sessionKey, ip)
+local confidenceKey = "confidence"
+local numVotesKey = "numVotes"
+local ipsKey = "ips"
+
+local confidence = tonumber(redis.call('GET', confidenceKey))
+local numVotes = tonumber(redis.call('GET', numVotesKey))
+local ipExists = redis.call('HEXISTS', ipsKey, ip)
 
 if ipExists == 1 then
-    local oldVote = tonumber(redis.call('HGET', sessionKey, ip))
-    avg = (avg * numVotes - oldVote + vote) / numVotes
+    local oldVote = tonumber(redis.call('HGET', ipsKey, ip))
+    if oldVote == vote then
+        return {tostring(confidence), tostring(numVotes)}
+    end
+    confidence = (confidence * numVotes - oldVote + vote) / numVotes
 else
-    redis.call('HINCRBY', sessionKey, 'numVotes', 1)
-    avg = (avg * numVotes + vote) / (numVotes + 1)
+    redis.call('INCR', numVotesKey)
+    confidence = (confidence * numVotes + vote) / (numVotes + 1)
     numVotes = numVotes + 1
 end
 
-redis.call('HSET', sessionKey, 'avg', tostring(avg))
-redis.call('HSET', sessionKey, ip, tostring(vote))
+redis.call('SET', confidenceKey, tostring(confidence))
+redis.call('HSET', ipsKey, ip, tostring(vote))
 
-return {tostring(avg), tostring(numVotes)}
+return {tostring(confidence), tostring(numVotes)}
 
